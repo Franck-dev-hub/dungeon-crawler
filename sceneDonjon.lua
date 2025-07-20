c441d773db6df1de5b683804c977ad61856a55fa
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-20 17:12:34 +0200

    Initialise sceneDonjon.lua structure

---
+++ sceneDonjon.lua
@@ -0,0 +1,26 @@
+-- Module creation
+local sceneDonjon = {}
+
+-- Load donjon scene
+function sceneDonjon.load()
+end
+
+-- Update donjon scne
+function sceneDonjon.update(dt)
+end
+
+-- Draw donjon scne
+function sceneDonjon.draw(dt)
+	love.graphics.print("Donjon scene")
+end
+
+-- Keyboard management
+function sceneDonjon.keypressed(key)
+end
+
+-- Mouse management
+function sceneDonjon.mousepressed(x, y, button, istouch)
+end
+
+-- Module return
+return sceneDonjon
