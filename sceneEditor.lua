b1706828eeda4642ec67a398a847d65ed7f31f0e
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-20 17:33:42 +0200

    Initialise sceneEditor.lua file

---
+++ sceneEditor.lua
@@ -0,0 +1,26 @@
+-- Module creation
+local sceneEditor = {}
+
+-- Load donjon scene
+function sceneEditor.load()
+end
+
+-- Update donjon scne
+function sceneEditor.update(dt)
+end
+
+-- Draw donjon scne
+function sceneEditor.draw(dt)
+	love.graphics.print("Editor scene")
+end
+
+-- Keyboard management
+function sceneEditor.keypressed(key)
+end
+
+-- Mouse management
+function sceneEditor.mousepressed(x, y, button, istouch)
+end
+
+-- Module return
+return sceneEditor
